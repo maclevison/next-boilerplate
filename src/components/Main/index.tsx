ea4c557d8b5ed1b4cd8f1f3a26fc4801e5e09a1e
Maclevison Silva <maclevison@gmail.com>
2021-05-12 21:25:33 +0100

    clean: remove example component

--- src/components/Main/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as S from './styles'
-
-const Main = () => (
-    <S.Wrapper>
-        <h1>Hello Boilerplate</h1>
-    </S.Wrapper>
-)
-
-export default Main
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,3 @@
-import Main from '../components/Main'
-
 export default function Home() {
-    return (
-        <div>
-            <Main />
-        </div>
-    )
+    return <div>Hello World</div>
 }
